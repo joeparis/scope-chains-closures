154ec82a1786608a1ac0579dc8e507fb5eac42fa
Joe Paris <joe.paris@outlook.com>
2016-11-02 21:53:25 -0700

    Finished garbage collection

---
+++ garbage.js
@@ -0,0 +1,11 @@
+function foo() {
+    var bar = 51;
+    quux = 42;
+
+    return zip;
+
+    function zip() {
+        bar = true;
+        var quux = 37;
+    }
+}
---
+++ global-scope-shadowing.js
@@ -0,0 +1,8 @@
+function foo() {
+    var bar = 51;
+    quux = 42;
+
+    function zip() {
+        var quux = 37;
+    }
+}
